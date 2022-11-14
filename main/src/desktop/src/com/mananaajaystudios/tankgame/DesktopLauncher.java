adad4a9a5d4f1d4f6874ef3b4433f63ffa8d6f94
sudokick <thedigitalmanan@gmail.com>
2022-11-14 10:12:48 +0530

    add static gui templates

--- main/src/desktop/src/com/mananaajaystudios/tankgame/DesktopLauncher.java
+++ main/src/desktop/src/com/mananaajaystudios/tankgame/DesktopLauncher.java
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("TankGame");
-		new Lwjgl3Application(new MyTankGame(), config);
+		new Lwjgl3Application(new MainPage(), config);
 	}
 }
