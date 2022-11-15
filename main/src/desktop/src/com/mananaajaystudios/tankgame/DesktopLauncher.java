b84b2c752a1d18310336f640ad988d96c04b44a7
sudokick <thedigitalmanan@gmail.com>
2022-11-16 00:24:51 +0530

    new classes

--- main/src/desktop/src/com/mananaajaystudios/tankgame/DesktopLauncher.java
+++ main/src/desktop/src/com/mananaajaystudios/tankgame/DesktopLauncher.java
@@ -1,8 +1,11 @@
 package com.mananaajaystudios.tankgame;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mananaajaystudios.tankgame.MyTankGame;
+import com.mananaajaystudios.tankgame.screens.MainPage;
+import jdk.tools.jmod.Main;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -10,6 +13,12 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("TankGame");
-		new Lwjgl3Application(new MainPage(), config);
+		new Lwjgl3Application(new GameInit(), config);
+	}
+}
+public class GameInit extends Game {
+	@Override
+	public void create () {
+		setScreen(new MainPage(this));
 	}
 }
