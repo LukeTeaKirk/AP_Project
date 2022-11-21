72121bd57aea99a9a826ac61ab2368f8f313a371
sudokick <thedigitalmanan@gmail.com>
2022-11-21 18:00:38 +0530

    linked all bbuttons

--- main/src/core/src/com/mananaajaystudios/tankgame/TopDog.java
+++ main/src/core/src/com/mananaajaystudios/tankgame/TopDog.java
@@ -6,12 +6,11 @@ import com.mananaajaystudios.tankgame.screens.*;
 public class TopDog extends Game {
     private MainPage mainPage;
     private GameModeSelector gameMode;
-    private PauseMenu pauseMenu;
     private MyTankGame tankGame;
     private EndGame endGame;
     private TankSelectorPlayer2 tankSelectorp2;
     private TankSelectorPlayer1 tankSelectorp1;
-
+    private MyTankGame mytankGame;
     public MainPage init(){
         mainPage = new MainPage(this);
         return mainPage;
@@ -37,14 +36,14 @@ public class TopDog extends Game {
                 if(tankSelectorp1 == null) tankSelectorp1 = new TankSelectorPlayer1(this);
                 this.setScreen(tankSelectorp1);
                 break;
-            /*case "TANKP2":
-                if(pauseMenu == null) pauseMenu = new PauseMenu(this);
-                this.setScreen(pauseMenu);
+            case "TANKP2":
+                if(tankSelectorp2 == null) tankSelectorp2 = new TankSelectorPlayer2(this);
+                this.setScreen(tankSelectorp2);
                 break;
-            case "ENDGAME":
-                if(endGame == null) endGame = new EndGame(this);
-                this.setScreen(endGame);
-                break;*/
+            case "GAME":
+                if(mytankGame == null) mytankGame = new MyTankGame(this);
+                this.setScreen(mytankGame);
+                break;
         }
     }
 }
